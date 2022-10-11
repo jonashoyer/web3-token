2aebcc8d21a18e319198bc13ce57290c834a6d00
jonashoyer <12399677+jonashoyer@users.noreply.github.com>
2022-10-11 11:14:18 +0200

    Ethers use signer address

--- src/integrations/ethers/sign.ts
+++ src/integrations/ethers/sign.ts
@@ -2,11 +2,12 @@ import { Web3TokenSignOptions } from "../../types";
 import { sign as baseSign } from '../../sign';
 import type { Signer } from "@ethersproject/abstract-signer";
 
-export const sign = async (signer: Signer, account: string, options: Web3TokenSignOptions) => {
+export const sign = async (signer: Signer, options: Web3TokenSignOptions) => {
 
   const signingMethod = (message: string) => {
     return signer.signMessage(message);
   }
 
+  const account = await signer.getAddress();
   return baseSign(account, signingMethod, options)
 }
